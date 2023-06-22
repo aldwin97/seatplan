5db0280111dac4b242d8937be3a2ab1fdf5f6198
aldwin97 <rivera.aldwin.f@gmail.com>
2023-06-22 14:55:53 +0800

    first commit

---
+++ src/main/java/com/seatplanproj/seatplan/SeatplanApplication.java
@@ -0,0 +1,13 @@
+package com.seatplanproj.seatplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seatplanproj/seatplan/SeatplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatplanproj.seatplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
